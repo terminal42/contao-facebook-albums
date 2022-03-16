5a804b0e5381c43b930a111aa0f4acc7a7893828
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-03-16 13:59:37 +0100

    Add compatibility with PHP 8

--- src/Resources/contao/dca/tl_facebook_album_account.php
+++ src/Resources/contao/dca/tl_facebook_album_account.php
@@ -72,7 +72,7 @@ $GLOBALS['TL_DCA']['tl_facebook_album_account'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_facebook_album_account']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
